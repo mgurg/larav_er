1e09cfcf195433820406f0226a0d5b116b5098bf
mgurg <34517378+mgurg@users.noreply.github.com>
2024-07-31 17:19:23 +0200

    Remove CloudinaryLabs from 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,6 +13,3 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "Publishing cloudinary provider..."
-php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
